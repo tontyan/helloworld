0c0384fe9ea1f1c4318235f7cd1465ad3198a7d5
g15949tn <g15949tn@s30432.ACTSUDA>
2017-04-14 11:50:12 +0900

    文章を追加しました。

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -17,6 +17,7 @@ public class Helloworld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+        System.out.println("貢献したよ！");
     }
     
 }
